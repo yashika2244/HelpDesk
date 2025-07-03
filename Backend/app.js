2fe335ee4b99e8a3e6c77df82dda4b6bb08e18f6
Yashu Rajput <yashu@gmail.com>
2025-07-03 10:17:03 +0530

    10:16

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://help-desk-olive.vercel.app/',
+    'https://help-desk-olive.vercel.app',
   ],
   credentials: true,
 }));
